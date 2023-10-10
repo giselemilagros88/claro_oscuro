897b813024749b3380552ea3d2326024da5f8ad9
giselemilagros88 <92891602+giselemilagros88@users.noreply.github.com>
2023-10-10 19:46:19 -0300

    master ajustes al tema dark

--- js/app.js
+++ js/app.js
@@ -1,15 +1,31 @@
-document.addEventListener('DOMContentLoaded',function() {
+document.addEventListener('DOMContentLoaded', function() {
     const switcherTheme = document.querySelector('.main__check');
     const root = document.documentElement;
+
+    // Verificar si el tema predeterminado del navegador es oscuro
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        root.setAttribute('data-theme', 'dark');
+        switcherTheme.checked = true;
+    }else{
+        root.setAttribute('data-theme', 'light');
+        switcherTheme.checked = false;
+    }
+
+    // Resto del código para el interruptor de tema
     switcherTheme.addEventListener('click', toggleTheme);
-    switcherTheme.addEventListener('touchstart', toggleTheme);
-    if(root.getAttribute('data-theme') === 'dark'){
-        switcherTheme.checked = true;      
-    } 
+
     function toggleTheme() {
         const setTheme = switcherTheme.checked ? 'dark' : 'light';
-        root.setAttribute('data-theme',setTheme);
-        localStorage.setItem('theme',setTheme);
-    };
-
+        root.setAttribute('data-theme', setTheme);
+        localStorage.setItem('theme', setTheme);
+    }
 });
+
+// Resto del contenido del archivo script.js
+const storageTheme = localStorage.getItem('theme');
+const systemColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+
+const newTheme = storageTheme ?? systemColorScheme;
+
+document.documentElement.setAttribute('data-theme', newTheme);
+
--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@ const systemColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matc
 const newTheme = storageTheme ?? systemColorScheme;
 
 document.documentElement.setAttribute('data-theme', newTheme);
+
+/*document.addEventListener('DOMContentLoaded',function() {
+    const switcherTheme = document.querySelector('.main__check');
+    const root = document.documentElement;
+    if(root.getAttribute('data-theme') === 'dark'){
+        switcherTheme.checked = true;      
+    } 
+    switcherTheme.addEventListener('click', toggleTheme);
+    switcherTheme.addEventListener('touchstart', toggleTheme);
+  
+    function toggleTheme() {
+        const setTheme = switcherTheme.checked ? 'dark' : 'light';
+        root.setAttribute('data-theme',setTheme);
+        localStorage.setItem('theme',setTheme);
+    };
+
+});*/
