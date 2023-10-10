c43918e21f1fb66ab7a31567931383ebad1ea702
giselemilagros88 <92891602+giselemilagros88@users.noreply.github.com>
2023-10-10 18:39:45 -0300

    agrego touchstart

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded',function() {
     const switcherTheme = document.querySelector('.main__check');
     const root = document.documentElement;
     switcherTheme.addEventListener('click', toggleTheme);
+    switcherTheme.addEventListener('touchstart', toggleTheme);
     if(root.getAttribute('data-theme') === 'dark'){
         switcherTheme.checked = true;      
     } 
